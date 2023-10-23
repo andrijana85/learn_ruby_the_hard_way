f2d63729c28d9147978ef035114f9457147e1437
andrijana85 <topalovic.andrijana@yahoo.com>
2023-10-23 11:26:58 -0400

    Exercises 3-6

---
+++ ex6.rb
@@ -0,0 +1,26 @@
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+# This is a string inside a string
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+# This is a boolean
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+# This is a string inside a string
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
+
+
